7eae40f5c4c7d6726c00ece5c5388df0051a3da4
vinicius barbosa <vinicius.barbosa.709@ufrn.edu.br>
2024-08-29 16:30:48 -0300

    Aprendendo [useState, useEffect, useMemo]

--- app1/src/app/pages/login/Login.tsx
+++ app1/src/app/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export const Login = () => {
   
@@ -6,6 +6,34 @@ export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    /*if (window.confirm('Voce é homem?')){
+      console.log("Homem")
+    } else {
+      console.log("Mulher")
+  
+    }*/
+  }, []);
+
+  useEffect(() => {
+    console.log(email)
+  }, [email]);
+
+  useEffect(() => {
+    console.log(password)
+  }, [password]);
+
+
+  /* usar useEffect para algo que só precisa ser chamado uma vez no código, como uma api com dados de usuario 
+
+  dependecies [x] = quando x for alterado, o codigo dentro do useEffect roda
+  */
+
+  const emailLength = useMemo(() => {
+    console.log("rodou")
+    return email.length * 10;
+  }, [email.length]);
+ 
 
   const handlerEntrar = () => {
     console.log(email)
@@ -18,6 +46,7 @@ export const Login = () => {
   return(
       <div>
         <form>
+          <p>Quantidade de caracteres do email: {emailLength}</p>
 
           <label>
             <span>Email</span>
